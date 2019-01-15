627d72427d9b6b80ff88232512fb6dbfb370b647
Apple <>
2019-01-14 18:43:35 -0800

    Initial commit.

---
+++ Pages/Table of Contents.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Table of Contents
+*/
+/*:
+ # Creating a Model from Tabular Data
+ ## Table of Contents
+ - [Train a Regressor and a Classifier](Train%20a%20Regressor%20and%20a%20Classifier)
+ - [LICENSE](LICENSE)
+ ****
+ [Next](@next)
+ */
